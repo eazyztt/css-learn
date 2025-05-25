c766c237b4eb03d0d525fa9903f8c21e551e9d42
Stsiapan Palianski <emslim66@gmail.com>
2025-05-25 21:51:42 +0300

    nav

--- yeah/src/components/typeMessageAndChat/tgLogin.jsx
+++ yeah/src/components/typeMessageAndChat/tgLogin.jsx
@@ -1,7 +1,9 @@
 // components/TelegramLogin.js
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function TelegramLogin() {
+  const navigate = useNavigate();
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?7";
@@ -25,7 +27,7 @@ export default function TelegramLogin() {
         .then((res) => res.json())
         .then((data) => {
           console.log("Авторизован!", data);
-          // например, сохранить пользователя или редирект
+          navigate("/");
         })
         .catch((err) => {
           console.error("Ошибка авторизации:", err);
