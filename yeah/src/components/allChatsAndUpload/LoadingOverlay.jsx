466146fc3e71208cc8bb32a9f76a9e960af7ed2f
Stsiapan Palianski <emslim66@gmail.com>
2025-06-02 21:35:42 +0300

    TEXT

--- yeah/src/components/allChatsAndUpload/LoadingOverlay.jsx
+++ yeah/src/components/allChatsAndUpload/LoadingOverlay.jsx
@@ -1,6 +1,6 @@
 import { Loader2 } from "lucide-react";
 
-export default function LoadingOverlay(text) {
+export default function LoadingOverlay({ text }) {
   return (
     <div className="fixed inset-0 z-50 flex flex-col gap-5 items-center justify-center bg-black/40 backdrop-blur-sm pointer-events-auto">
       <Loader2 className="h-12 w-12 animate-spin text-white" />
